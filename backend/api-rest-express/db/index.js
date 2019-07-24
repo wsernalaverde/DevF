b60be1e732dba1200712a54b3b29309c4a8522c2
William Serna <wserna@esquema.com>
2019-07-24 14:43:11 -0500

    Se crea endpoints para agregar usuario y obtener usuarios en el ejercicio api-rest-express

--- backend/api-rest-express/db/index.js
+++ backend/api-rest-express/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const db_url = 'mongodb+srv://user-exercises:abcd1234@cluster0-imbkw.mongodb.net/test?retryWrites=true&w=majority'
+const db_url = 'mongodb+srv://user-exercises:abcd1234@cluster0-imbkw.mongodb.net/api-express?retryWrites=true&w=majority'
 
 mongoose.connect(
   db_url,
--- backend/api-rest-express/server/index.js
+++ backend/api-rest-express/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
+const User = require('../models/User')
 
 const app = express()
 
@@ -12,6 +13,19 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.status(200).send('¡Hola Mundo!')
+})
+
+app.get('/api/v1/users', (req, res) => {
+  User.find()
+    .then(users => res.status(200).send(users))
+    .catch(err => res.status(400).send(err))
+})
+
+app.post('/api/v1/users', (req, res) => {
+  const newUser = new User(req.body)
+  newUser.save()
+    .then(user => res.status(201).send(user))
+    .catch(err => res.status(400).send(err))
 })
 
 module.exports = { app, PORT }

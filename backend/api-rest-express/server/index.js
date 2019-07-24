1146517bfde47803c6790d2caae3d19aa11bb7da
William Serna <wserna@esquema.com>
2019-07-24 14:26:59 -0500

    Se agrega middleware bodyparser en el ejercicio api-rest-express

--- backend/api-rest-express/server/index.js
+++ backend/api-rest-express/server/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.status(200).send('¡Hola Mundo!')
